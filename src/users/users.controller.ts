4dd4a607b46c9d3178031ab7fde01aee4807046b
idris agboola <agboolaisholaidreez@gmail.com>
2022-02-06 23:39:23 +0100

    update

---
+++ src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserDto } from './user.dto';
+import { UsersService } from './users.service';
+interface UserProps {
+  id: number;
+  name: string;
+}
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  getUsers(): UserProps[] {
+    return this.userService.getAllUser();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): UserProps {
+    return this.userService.getUserById(parseFloat(id));
+  }
+
+  @Post()
+  createUser(@Body() body: UserDto): any {
+    return body;
+  }
+}
