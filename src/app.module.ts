38b5e2ff7bb4321fffb1de55a8c52f74662c0264
idris agboola <agboolaisholaidreez@gmail.com>
2022-02-07 21:58:28 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,8 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from '../ormconfig';
-import User from './users/users.entity';
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forRoot(config),
-    TypeOrmModule.forFeature([User]),
-  ],
+  imports: [UsersModule, TypeOrmModule.forRoot(config)],
 })
 export class AppModule {}
