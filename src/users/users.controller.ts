a39aeecc7aa7a8918273d9fcb95dfd530650c046
idris agboola <agboolaisholaidreez@gmail.com>
2022-02-07 21:43:13 +0100

    update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,19 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
+import User from './users.entity';
 import { UsersService } from './users.service';
-interface UserProps {
-  id: number;
-  name: string;
-}
 
 @Controller('users')
 export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Get()
-  getUsers(): UserProps[] {
+  getUsers(): Promise<User[]> {
     return this.userService.getAllUser();
   }
 
   @Get(':id')
-  getUserById(@Param('id') id: string): UserProps {
+  getUserById(@Param('id') id: string): Promise<User> {
     return this.userService.getUserById(parseFloat(id));
   }
 
