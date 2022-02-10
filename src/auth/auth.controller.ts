1461a105c69ea01f43ca4fd4e8dcd0a552b28cda
idris agboola <agboolaisholaidreez@gmail.com>
2022-02-10 23:04:02 +0100

    update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,41 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UnauthorizedException,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto, ResDto } from './dto/create-auth.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() createAuthDto: CreateAuthDto): Promise<ResDto> {
+  async register(
+    @Body() createAuthDto: CreateAuthDto,
+    @Res() res: Response,
+  ): Promise<ResDto> {
     try {
       const res = await this.authService.validateRegisterInput(createAuthDto);
-      return { msg: res };
+      if (res) return { msg: 'vvv' };
+      return { msg: 'jjjj' };
     } catch (error) {
-      return {
-        msg: error.message,
-      };
+      if (error.code === '23505') {
+        if (error.detail.includes('email'))
+          res.status(401).json({
+            email: 'email already exist',
+          });
+        if (error.detail.includes('username'))
+          res.json({
+            username: 'username already exist',
+          });
+      } else {
+        res.status(400).json({ error });
+      }
     }
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import UserRepository from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
 import { validate } from 'class-validator';
@@ -12,24 +11,23 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private UserRepository: Repository<UserRepository>,
   ) {}
-  async validateRegisterInput(data: CreateAuthDto): Promise<string> {
-    const user = this.UserRepository.create(data);
 
-    const errors = await validate(user);
+  async validateRegisterInput(data: CreateAuthDto): Promise<boolean> {
+    const newuser = this.UserRepository.create(data);
+
+    const errors = await validate(newuser);
     if (errors.length > 0) {
-      // const err = {};
-      // errors.forEach((err) => {
-      //   const key = err.property;
-
-      //   if (err.constraints) {
-      //     const value = Object.entries(err.constraints)[0][1];
-      //     err[key] = value;
-      //   }
-      // });
-
-      return 'validate err';
+      const res: any = {};
+      errors.forEach((err) => {
+        const key = err.property;
+        if (err.constraints) {
+          const value = Object.entries(err.constraints)[0][1];
+          res[key] = value;
+        }
+      });
+      throw res;
     }
 
-    return 'validate suc';
+    return true;
   }
 }
