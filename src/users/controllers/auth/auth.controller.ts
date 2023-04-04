cc90f3a7828b95b964c64dd7e82e4c186e422488
Iris <agboolaisholaidreez>
2023-04-04 01:29:03 +0100

    update

--- src/users/controllers/auth/auth.controller.ts
+++ src/users/controllers/auth/auth.controller.ts
@@ -1,18 +1,23 @@
 import { Controller, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+
 @Controller('auth')
 export class AuthController {
   @Post('signup')
   signUp(@Req() req: Request, @Res() res: Response) {
-    res.cookie('cookie', 'ddgdgdgdggdgdg');
+    res.cookie('cookie', 'ddgdgdgdggdgdg', {
+      expires: new Date(Number(new Date()) + 1000 * 60 * 60),
+    });
     res.json({ message: 'Hello world' });
     return { message: 'register successfull', user: req.body };
   }
 
   @Post('logout')
   signOut(@Req() req: Request, @Res() res: Response) {
-    console.log(req);
-    if (!req.cookies) res.status(500).json({ message: 'unauthorized' });
-    res.json({ message: 'logout' });
+    if (!req.cookies?.cookie)
+      return res.status(500).json({ message: 'unauthorized' });
+
+    res.clearCookie('cookie');
+    return res.json({ message: 'logout' });
   }
 }
