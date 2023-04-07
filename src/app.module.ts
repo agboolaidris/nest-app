3920a617bd75e92bed7fb7fc9bf90e4d5b029942
Iris <agboolaisholaidreez>
2023-04-07 11:50:33 +0100

    update

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'postgres',
+      password: '123',
+      database: 'db',
+      synchronize: true,
+      logging: true,
+      autoLoadEntities: true,
+    }),
+    UsersModule,
+    PostsModule,
+  ],
+})
+export class AppModule {}
