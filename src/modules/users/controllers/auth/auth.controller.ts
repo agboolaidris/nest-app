2241d0a79dab4206a5055be9c46a065d6c442a2f
Iris <agboolaisholaidreez>
2023-04-08 01:48:23 +0100

    signin

--- src/modules/users/controllers/auth/auth.controller.ts
+++ src/modules/users/controllers/auth/auth.controller.ts
@@ -10,6 +10,7 @@ import {
 import { SignUpDto } from '../../dtos/sign-up.dto';
 import { AuthService } from '../../services/auth/auth.service';
 import { PasswordValidationPipe } from '../../pipes/password-validation/password-validation.pipe';
+import { SignInDto } from '../../dtos/sign-in.dto';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -23,5 +24,11 @@ export class AuthController {
   )
   async Signup(@Body() signUpDto: SignUpDto) {
     return this.authService.SignUp(signUpDto);
+  }
+
+  @Post('/signin')
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async Signin(@Body() signInDto: SignInDto) {
+    return signInDto;
   }
 }
